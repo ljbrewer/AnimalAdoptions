41682feed53846457daa6e6faae81f47b4494795
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-07-28 19:22:23 -0700

    Bears css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,4 @@
-var searchButton = document.getElementById('find-me');
 
-
-function displayBear()
-
-
-function getBearPics(term = 'bears') {
-    
- var key = '21fdcacl68aypKOUNa7JaHMbnkOLR7Z7';
-var endPoint = 'https://api.giphy.com/v1/gifs/search?limit=1&api_key=' + key + "&q=" + term;
-fetch(endPoint)
-.then(function(response) {
-    return response.json(); 
-})
-.then(function(data) {
-    displayGif(data.data[0]);
-})  
-}
-
-
-searchButton.addEventListener('click', getBearPics);
 
 
 
