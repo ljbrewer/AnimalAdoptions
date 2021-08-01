93759abd66d9db65279258ce602caf3ccde12065
Damali <brown@MacBook-Pro.local>
2021-08-01 15:08:34 -0700

    update

--- assets/js/cat2.js
+++
@@ -1,8 +0,0 @@
-function start() {
-    var caturl = localStorage.getItem(2)
-    // console.log(caturl)
-    var cat1 = document.getElementById("catImg2");
-    cat1.setAttribute('src', caturl)
-}
-
-start();
