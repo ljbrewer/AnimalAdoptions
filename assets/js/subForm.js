15cbb87b0de9ee653da917635b3217c5bda4c91c
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-08-02 18:52:44 -0700

    submit button

--- assets/js/subForm.js
+++ assets/js/subForm.js
@@ -1,13 +1,27 @@
-var submissionForm = document.querySelector('')
+var submissionForm = document.querySelector('.submit-Info');
+var submittedInfo = []
+var congratsEl = document.querySelector('#congrats'); 
+
+// var adoptionInfo = localStorage.get('')
 
 function submitForm() {
-  if(document.adoptForm.onsubmit && !document.adoptForm.onsubmit()) {
-    return;
-  }
-  document.adoptForm.submit();
+  submissionForm.style.display = 'block';
+  congratsEl.style.display = 'none';
+
+  var firstName = localStorage.getItem('multi-first-name')
+  var lastName = localStorage.getItem('multi-last-name')
+  var email = localStorage.getItem('multi-email')
+  var phoneNumber = localStorage.getItem('multi-phone-number')
+  var address = localStorage.getItem('multi-address')
+  var city = localStorage.getItem('multi-city')
+  var state = localStorage.getItem('multi-state')
+  var zipCode = localStorage.getItem('multi-zip-code')
+  var yesNo = localStorage.getItem('selectionYN')
 
 
 } 
+
+
 
 
 
